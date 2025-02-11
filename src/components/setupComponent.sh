00ab0c0923c024965c0a2fe4405fef05ef7afc2b
Boaz Kaufman <bk1nyc@gmail.com>
2025-02-11 01:09:42 -0500

    Write bash script to set up component folder quickly; set up several component folders quickly

---
+++ src/components/setupComponent.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Reset in case getopts has been used previously in the shell.
+OPTIND=1
+
+# Initialize our own variables:
+dirname=""
+
+while getopts "n:" opt; do
+  case "$opt" in
+    n)  dirname=$OPTARG
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+echo "dirname='$dirname', Leftovers: $@"
+
+mkdir $dirname
+cd $dirname
+touch ${dirname}.jsx ${dirname}.test.js ${dirname}.css
+echo "import './"${dirname}".css'" >> ${dirname}.jsx
+cd ..
