1ca82b75cd6ab92aaecbaa9ef54eda79339524e4
Boaz Kaufman <bk1nyc@gmail.com>
2025-02-15 17:21:54 -0500

    Remove flattened nav links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -45,71 +45,31 @@ function NavBarSubLinks ({ subroutes }){
   
 function NavBar () {
 
-  const flat_routes = {}
-  Object.keys(nav_routes)
-    .forEach((link) => {
-      if (typeof nav_routes[link] === 'string') {
-        flat_routes[link] = nav_routes[link];
-        return;
-      }
-      else if (typeof nav_routes[link] === 'object') {
-        Object.keys(nav_routes[link])
-          .forEach((sublink) => {
-            if (sublink === 'main') {
-              flat_routes[link] = nav_routes[link][sublink];
-            }
-            else {
-              flat_routes[sublink] = nav_routes[link][sublink];
-            }
-            return;
-          });
-        return;
-      }
-    });
-  console.log(flat_routes)
-
   return (
-    <>
-      <div className="nav-wrapper">
-        {
-          Object.keys(nav_routes)
-            .map((link) => {
-              return (
-                <>
-                  <NavBarLink
-                    text={link}
-                    href={typeof nav_routes[link] === 'string'
-                      ? nav_routes[link]
-                      : nav_routes[link].main}
-                    key={link}
-                  >
-                    {
-                      typeof nav_routes[link] === 'object'
-                      && (<NavBarSubLinks subroutes={nav_routes[link]} />)
-                    }
-                  </NavBarLink>
-                </>
-              )
-            }
-          )
-        }
-      </div>
-      <div className="nav-wrapper-flat">
-        {
-          Object.keys(flat_routes)
-            .map((link) => {
-              return (
+    <div className="nav-wrapper">
+      {
+        Object.keys(nav_routes)
+          .map((link) => {
+            return (
+              <>
                 <NavBarLink
                   text={link}
-                  href={flat_routes[link]}
+                  href={typeof nav_routes[link] === 'string'
+                    ? nav_routes[link]
+                    : nav_routes[link].main}
                   key={link}
-                />
-              )
-            }
-          )
-        }
-      </div>
-    </>
+                >
+                  {
+                    typeof nav_routes[link] === 'object'
+                    && (<NavBarSubLinks subroutes={nav_routes[link]} />)
+                  }
+                </NavBarLink>
+              </>
+            )
+          }
+        )
+      }
+    </div>
   )
 }
 
