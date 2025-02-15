69c11c00eb4b3078b0670de9dad91a002fe523a3
Boaz Kaufman <bk1nyc@gmail.com>
2025-02-15 17:57:50 -0500

    Make header icon a link to home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import './Header.css'
 import logo from './Lota Logo White.avif'
 import NavBar from '../NavBar/NavBar';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header-wrapper">
-      <img src={logo} alt="LOTA logo" />
+      <Link to='/'>
+        <img src={logo} alt="LOTA logo" />
+      </Link>
       <NavBar />
     </div>
   );
