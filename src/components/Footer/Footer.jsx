3244a2681ed068007df10663a556ad6d3fa87a63
Boaz Kaufman <bk1nyc@gmail.com>
2025-02-21 16:42:30 -0500

    Start working on fixed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,19 @@
 import NavBar from '../NavBar/NavBar';
 import './Footer.css';
 
+function ContactList() {
+  return (
+    <div className="contactlist-wrapper">
+      
+    </div>
+  );
+}
+
 function Footer () {
   return (
     <div className='footer-wrapper'>
-      <NavBar />
+      <h2>Contact us!</h2>
+      <ContactList />
     </div>
   );
 }
