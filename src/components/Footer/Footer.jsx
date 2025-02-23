a02db7a0c056bde74928860b8152c9dff5d58dba
Boaz Kaufman <bk1nyc@gmail.com>
2025-02-22 21:12:56 -0500

    Set up contactlist in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,33 @@ import NavBar from '../NavBar/NavBar';
 import './Footer.css';
 
 function ContactList() {
+  contacts = (
+    {
+      label: 'email',
+      link: '',
+      icon: '',
+    },
+    {
+      label: 'instagram',
+      link: '',
+      icon: '',
+    },
+    {
+      label: 'bluesky',
+      link: '',
+      icon: '',
+    },
+    {
+      label: 'venmo',
+      link: '',
+      icon: '',
+    }
+  );
   return (
     <div className="contactlist-wrapper">
-      
+      {contacts.map(() => {
+        <Link to={contact.link}>contact.label</Link>
+      })}
     </div>
   );
 }
