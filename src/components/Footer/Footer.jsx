cf67f5d96e3225fc7330a75abd60e3dcaf0217e7
Boaz Kaufman <bk1nyc@gmail.com>
2025-02-28 17:03:02 -0500

    Style footer and add links to contacts

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import InstagramIcon from './ContactIcons/instagram-svgrepo-com.svg';
-import BlueskyIcon from './ContactIcons/bluesky-stroke-rounded.svg';
+// import BlueskyIcon from './ContactIcons/bluesky-stroke-rounded.svg';
 import VenmoIcon from './ContactIcons/icons8-venmo-50.png';
 
 import { Link } from 'react-router-dom';
@@ -9,23 +9,23 @@ function ContactList() {
   const contacts = [
     {
       label: 'email',
-      link: '',
+      link: 'mailto:libraryoftransalexander@proton.me',
     },
     {
       label: 'instagram',
-      link: '',
+      link: 'https://www.instagram.com/libraryoftransalexander/',
       icon: InstagramIcon,
       alt: 'Instagram icon',
     },
-    {
-      label: 'bluesky',
-      link: '',
-      icon: BlueskyIcon,
-      alt: 'Blue Sky icon',
-    },
+    // {
+    //   label: 'bluesky',
+    //   link: '',
+    //   icon: BlueskyIcon,
+    //   alt: 'Blue Sky icon',
+    // },
     {
       label: 'venmo',
-      link: '',
+      link: 'https://account.venmo.com/u/libraryoftransalexander',
       icon: VenmoIcon,
       alt: 'Venmo icon',
     }
@@ -33,7 +33,7 @@ function ContactList() {
   return (
     <div className="contactlist-wrapper">
       {contacts.map(( contact ) => (
-        <Link to={contact.link}>
+        <Link target='_blank' rel='noreferrer' to={contact.link}>
           {
             contact.icon && contact.alt
               ? (<img src={contact.icon} alt={contact.alt}/>)
