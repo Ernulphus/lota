a0ed49be0fd5adc62e416f52aafdc46122a6e4c2
Boaz Kaufman <bk1nyc@gmail.com>
2025-03-28 18:50:00 -0400

    Fix lint errors on newsletter signup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -50,42 +50,68 @@ function Footer () {
   return (
     <div className='footer-wrapper'>
       <h1>Contact us!</h1>
-      <Newsletter />
+      <NewsletterSignupForm />
       <ContactList />
     </div>
   );
 }
 
-function Newsletter() {
-<form action="https://app.keila.io/forms/nfrm_dj3jymRJ" class="contact-form container bg-white rounded py-4 md:py-8 flex flex-col gap-4" method="post" style="background-color:#f9fafb!important;color:#111827!important">
-  <h1 class="text-4xl my-4">
-    Sign up for our Newsletter!
-  </h1>
-  <div class="flex flex-col">
-    <label for="contact_email">
-      Email
-    </label>
-    <input id="contact_email" name="contact[email]" style="background-color:#ffffff!important;border-color:#6b7280!important;color:#111827!important" type="email"/>
-  </div>
-  <div class="flex flex-col">
-    <label for="contact_first_name">
-      First name (optional)
-    </label>
-    <input id="contact_first_name" name="contact[first_name]" style="background-color:#ffffff!important;border-color:#6b7280!important;color:#111827!important" type="text"/>
-  </div>
-  <input aria-hidden="true" autocomplete="off" name="h[url]" novalidate="" style="display: none"/>
-  <div class="flex flex-col">
-    <div class="h-captcha" data-sitekey="a1c5fab7-71df-4ea4-af75-87a3d07c61de" data-theme="dark">
-    </div>
-    <script async="" defer="" src="https://hcaptcha.com/1/api.js">
-    </script>
-  </div>
-  <div class="flex justify-start">
-    <button class="button button--cta button--large" style="background-color:#047857!important;color:#f9fafb!important">
-      Submit
-    </button>
-  </div>
-</form>
+function NewsletterSignupForm() {
+  return (
+    <form
+      action="https://app.keila.io/forms/nfrm_dj3jymRJ"
+      className="contact-form"
+      method="post"
+      style={{background_color: '#f9fafb', color:'#111827'}}
+    >
+      <h1>
+        Sign up for our Newsletter:
+      </h1>
+      <div>
+        <label for="contact_email">
+          Email
+        </label>
+        <input
+          id="contact_email"
+          name="contact[email]"
+          style={{background_color: '#ffffff', border_color: '#6b7280', color:'#111827'}}
+          type="email"
+        />
+      </div>
+      <div>
+        <label for="contact_first_name">
+          First name (optional)
+        </label>
+        <input
+          id="contact_first_name"
+          name="contact[first_name]"
+          style={{background_color: '#ffffff', border_color: '#6b7280', color: '#111827'}}
+          type="text"/>
+      </div>
+      <input
+        aria-hidden="true"
+        autocomplete="off"
+        name="h[url]"
+        novalidate=""
+        style={{display: 'none'}}
+      />
+      <div>
+        <div
+          class="h-captcha"
+          data-sitekey="a1c5fab7-71df-4ea4-af75-87a3d07c61de"
+          data-theme="dark"
+        >
+        </div>
+        <script async="" defer="" src="https://hcaptcha.com/1/api.js">
+        </script>
+      </div>
+      <div>
+        <button class="button button--cta button--large" style={{background_color: '#047857', color: '#f9fafb'}}>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default Footer;
